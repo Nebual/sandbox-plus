cd39daf91d20a565b37df91e412471485634e521
CaptainSaveACode <captainsaveacode@gmail.com>
2023-09-02 11:34:13 -0500

    Add hint feed for undo/redo.

---
+++ code/ui/HintFeed/HintFeed.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+using Sandbox.Tools;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System;
+using System.Xml.Linq;
+
+public partial class HintFeed : Panel
+{
+	static HintFeed Current;
+
+	public HintFeed()
+	{
+		StyleSheet.Load( "ui/HintFeed/HintFeed.scss" );
+		Current = this;
+	}
+
+	[ClientRpc]
+	public static void AddHint( string msg, bool redo = false )
+	{
+		var e = Current.AddChild<HintFeedEntry>();
+
+		var undoRedo = redo ? "redo" : "undo";
+		e.Icon = e.Add.Icon(undoRedo, $"icon {undoRedo}");
+		e.Name = e.Add.Label( msg, "msg" );
+	}
+}
