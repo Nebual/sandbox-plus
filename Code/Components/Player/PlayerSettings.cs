50366493173f04a41b78fe8e5dc281feb5930203
Craig McKay <developer@craig.mckay.kiwi>
2025-01-03 13:07:56 +1300

    Add customisable crosshairs! Woo. Necessary, no. Cool? Maybe lol.

--- Code/Components/Player/PlayerSettings.cs
+++ Code/Components/Player/PlayerSettings.cs
@@ -18,6 +18,21 @@ namespace Sandbox
 			}
 		}
 
+		public static string CustomCrosshairDefault = "(circle;255;255;255;0;0;5;5)";
+		private string customCrosshair = CustomCrosshairDefault;
+		public string CustomCrosshair
+		{
+			get => customCrosshair;
+			set
+			{
+				if ( customCrosshair == value ) return;
+				customCrosshair = value;
+				SetDirty();
+
+				if (player != null) player.RegenerateCrosshair();
+			}
+		}
+
 		// This is a bit overengineered heh; outer code can just set Player.Local.Settings.UseHumanModel = true, and after 5 seconds it will be saved.
 		// Multiple writes to PlayerSettings in that period will be batched together, and SetDirty(false) will cancel the save.
 		private void SetDirty( bool dirty = true )
@@ -48,6 +63,7 @@ namespace Sandbox
 		public static PlayerSettings Load()
 		{
 			var loaded = FileSystem.Data.ReadJson<PlayerSettings>( "playersettings.json" ) ?? new PlayerSettings();
+			loaded.player = Player.FindLocalPlayer();
 			loaded.SetDirty( false );
 			return loaded;
 		}
